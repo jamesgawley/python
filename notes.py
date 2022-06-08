87ff35e2b9b7150ee67a589b151a8d2b308d05f9
jamesgawley <james.gawley@gmail.com>
2022-06-08 16:51:03 +0200

    notes update

--- notes.py
+++ notes.py
@@ -1,20 +1,41 @@
-
-class Warrior:
-     def __init__(self, name, age):
-             self.name = name
-             self.age = age
-             self.strength = 10
-
-
-
-
 # the problem yesterday with the BattleMage class inheritance was that the inheritance class order was not considered.
 # the BattleMage class looked for __init__ in itself, and when it found that, it overrode the __init__ method from the parent class.
 # __init__ is called whenever a class definition is called to create a new object
 
 
 # open a file
-open()
+file = open("sample2.txt")
+
+sentenceList = file.readlines()
+
+#ASSIGNMENT:
+'''
+Open a text file.
+
+
+Read in all of its text (one chunk at a time)
+Split each chunk of text into sentences by the "." character.
+Measure the length of each sentence.
+Count the number of sentences whose length you have measured.
+Sum the sentence lengths and divide that by the number of sentences.
+
+'''
+
+file = open("sample2.txt", encoding="utf=8")
+
+chunkList = file.readlines()
+
+numberOfSentences = 0
+length = 0
+
+for chunk in chunkList:
+        sentences = chunk.split(".")
+        for i in sentences:
+            numberOfSentences = numberOfSentences + 1
+            length = length + len(i.split())
+
+result = length/numberOfSentences
+
 
 # make a list of sentences
 
